65e20a52fee5df4402089235c277f3db1629a14d
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-05-03 17:11:55 -0300

    Add - Campos no cadastro do usuarios

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,150 @@
 const mongoose = require( '../database/index.js' )
 
 const UserSchema = new mongoose.Schema( {
+
     name:{ type: String },
-    email:{ type: String },
+    email:{ type: String, unique: true },
+    rank:{
+        points: [ Number ],
+        position: Number,
+        achievements:[{
+            type: String,
+            name: String,
+            details: String,
+            acquiredPoints: Number,
+            medal: String,
+            initDate: Date,
+            endDate: Date
+        }]
+
+    },
+    planning: {
+        
+        commercial: {
+            questions: [{
+                question: String,
+                answer: String,
+            }],
+            items:[{
+                name: String,
+                impactPoints: String
+            }],
+            running:[{
+                type: String,
+                name: String,
+                impactPoints: String,
+                status: String,
+                responsible: String,
+                initDate: Date,
+                endDate: Date,
+            }]
+        },
+
+        marketing: {
+            questions: [{
+                question: String,
+                answer: String,
+            }],
+            items:[{
+                name: String,
+                impactPoints: String
+            }],
+            running:[{
+                type: String,
+                name: String,
+                impactPoints: String,
+                status: String,
+                responsible: String,
+                initDate: Date,
+                endDate: Date, 
+            }]
+        },
+         
+        operational: {
+            questions: [{
+                question: String,
+                answer: String,
+            }],
+            items:[{
+                name: String,
+                impactPoints: String
+            }],
+            running:[{
+                type: String,
+                name: String,
+                impactPoints: String,
+                status: String,
+                responsible: String, 
+                initDate: Date,
+                endDate: Date, 
+            }]
+        },
+         
+        financial: {
+            questions: [{
+                question: String,
+                answer: String,
+            }],
+            items:[{
+                name: String,
+                impactPoints: String
+            }],
+            running:[{
+                type: String,
+                name: String,
+                impactPoints: String,
+                status: String,
+                responsible: String, 
+                initDate: Date,
+                endDate: Date, 
+            }]
+        },
+         
+        legislation: {
+            questions: [{
+                question: String,
+                answer: String,
+            }],
+            items:[{
+                name: String,
+                impactPoints: String
+            }],
+            running:[{
+                type: String,
+                name: String,
+                impactPoints: String,
+                status: String,
+                responsible: String,
+                initDate: Date,
+                endDate: Date,  
+            }]
+        },
+         
+        management: {
+            questions: [{
+                question: String,
+                answer: String,
+            }],
+            items:[{
+                name: String,
+                impactPoints: String
+            }],
+            running:[{
+                type: String,
+                name: String,
+                impactPoints: String,
+                status: String,
+                responsible: String, 
+                initDate: Date,
+                endDate: Date, 
+            }]
+
+        },
+
+    },
+
+
+
 } )
 
 const User = mongoose.model( 'User', UserSchema )
