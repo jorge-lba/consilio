575709e59459a9c6d407abfa2527655e17955220
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-05-03 22:02:53 -0300

    Add - Procfile Heroku

---
+++ src/APIs/twilio/index.js
@@ -0,0 +1,29 @@
+const twilio = require( 'twilio' )
+require( 'dotenv/config' )
+
+const twilioClient = twilio( 
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
+)
+
+const sendMessageSMS = async ( sendTo ) => {
+
+    try {
+
+        const response  = await twilioClient.messages.create({
+
+            from: process.env.TWILIO_NUMBER,
+            to: sendTo || process.env.TWILIO_NUMBER_TEST,
+            body: 'Você pretende dormir essa madrugada? Consilio - Planejamos até seus SONHOS'
+
+        })
+
+        console.log( response )
+
+    } catch (error) {
+        console.log( error )
+    }
+
+}
+
+module.exports = sendMessageSMS
